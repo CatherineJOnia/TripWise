be7bc7642038d4818825b9e6c58fbc1bd8c44370
CatherineJOnia <catherinejonia@gmail.com>
2023-12-06 14:22:54 -0500

    Header.jsx initialized

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import Search from '@material-ui/icons/Search';
 
 const Header = () => {
   return (
-    <h1>Header</h1>
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography varient='h5' className={classes.title}>
+          TripWise
+        </Typography>
+        <Box display='flex'>
+          <Typography varient='h6' className={classes.title}>
+            Explore new places
+          </Typography>
+          <Autocomplete>
+            <div classname={classes.search}>
+              <div className={classes.searchIcon}>
+                <Search />
+              </div>
+              <InputBase placeholder="Search..." classes={{ root: classes.inputRoot, input: classes.inputInput }}/>
+            </div>
+          </Autocomplete>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 
