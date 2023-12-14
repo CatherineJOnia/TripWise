451bfd23d9c3fe96e797278b4cb336b3611dfad5
CatherineJOnia <catherinejonia@gmail.com>
2023-12-14 16:23:16 -0500

    remove console.logs and commented code

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,5 @@
 import axios from "axios";
 
-// const URL =
-//   "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
-
 export const getPlacesData = async (sw, ne) => {
   try {
     const {
@@ -15,10 +12,6 @@ export const getPlacesData = async (sw, ne) => {
           bl_longitude: sw.lng,
           tr_longitude: ne.lng,
           tr_latitude: ne.lat,
-          // bl_latitude: "11.847676",
-          // tr_latitude: "12.838442",
-          // bl_longitude: "109.095887",
-          // tr_longitude: "109.149359",
         },
         headers: {
           "x-rapidapi-key": process.env.REACT_APP_RAPID_API_TRAVEL_API_KEY,
