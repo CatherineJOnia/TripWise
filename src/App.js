dc5caae5fc481813a8211bbfcff43a62631deb5d
CatherineJOnia <catherinejonia@gmail.com>
2023-12-14 14:23:09 -0500

    map bug fixed

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
 import React, { useState, useEffect } from "react";
 import { CssBaseline, Grid } from "@material-ui/core";
 
-import { getPlacesData } from "./api/index.js";
+import { getPlacesData } from "./api";
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
 import Map from "./components/Map/Map";
 
 const App = () => {
   const [places, setPlaces] = useState([]);
-  const [coordinates, setCoordinates] = useState({});
+
+  const [coords, setCoords] = useState({});
   const [bounds, setBounds] = useState([]);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
-      ({ coordinates: { latitude, longitude } }) => {
-        setCoordinates({ lat: latitude, lng: longitude });
+      ({ coords: { latitude, longitude } }) => {
+        setCoords({ lat: latitude, lng: longitude });
       }
     );
   }, []);
 
   useEffect(() => {
+    console.log(coords, bounds);
+
     getPlacesData(bounds.sw, bounds.ne).then((data) => {
+      console.log(data);
       setPlaces(data);
     });
-  }, [coordinates, bounds]);
+  }, [coords, bounds]);
 
   return (
     <>
@@ -31,13 +35,23 @@ const App = () => {
       <Header />
       <Grid container spacing={3} style={{ width: "100%" }}>
         <Grid item xs={12} md={4}>
-          <List />
+          <List places={places} />
         </Grid>
-        <Grid item xs={12} md={8}>
+        <Grid
+          item
+          xs={12}
+          md={8}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
           <Map
-            setCoordinates={setCoordinates}
+            setCoords={setCoords}
             setBounds={setBounds}
-            coordinates={coordinates}
+            coords={coords}
+            places={places}
           />
         </Grid>
       </Grid>
